474ff640ebb854703cf4f3f34557db1f93e486a7
shaswift <shaswift19@gmail.com>
2023-07-26 21:38:33 +0100

    2nd commit

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var generateBtn = document.getElementById("generateBtn")
 var result = document.getElementById("result")
+var computerChoiceDisplay = document.getElementById("computerChoice");
 generateBtn.addEventListener("click", function(){
-   result.innerHTML= Math.floor(Math.random() * 3);
-} );
+   var randomNumber= Math.floor(Math.random() * 3);
+   result.innerHTML = randomNumber ;
+   if (randomNumber === 0) {
+      computerChoiceDisplay.innerHTML = "Computer chooses: Rock";
+  } else if (randomNumber === 1) {
+      computerChoiceDisplay.innerHTML = "Computer chooses: Paper";
+  } else if (randomNumber === 2) {
+      computerChoiceDisplay.innerHTML = "Computer chooses: Scissor";
+  }
+});
+
+
+
+
